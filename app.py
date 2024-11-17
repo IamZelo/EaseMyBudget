e1099bfd5a03023e6f01ba0b334d6eff99335a72
Harsh Panigrahi <harshpanigrahi@gmail.com>
2024-11-17 11:34:34 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,178 @@
+import os
+import json
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required, apology, get_expense, get_income, get_transaction_statment, inr, format_date
+
+# Configure application
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["inr"] = inr
+app.jinja_env.filters["formatDate"] = format_date
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///easebudget.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["password"], request.form.get("password")
+        ):
+            return apology("invalid username / password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        flash(f"Logged in as {rows[0]["username"]}")
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if username and password and (password == confirmation):
+            try:
+                db.execute("INSERT INTO users (username, password) VALUES (?,?)",
+                           username, generate_password_hash(password))
+            except ValueError:
+                return apology("Username already exists.")
+            return redirect("/login")
+        else:
+            return apology("Either input is blank or the passwords do not match.")
+
+    return render_template("register.html")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show Dashboard"""
+    id = session["user_id"]
+    #Last 7Days
+    weekly_expense = get_expense(id, 7)
+    monthly_expense = get_expense(id, 30)
+    halfyearly_expense = get_expense(id, 180)
+    
+    weekly_income = get_income(id,7)
+
+    transaction_statment_dict = get_transaction_statment(weekly_expense["expense_list"], weekly_income["income_list"])
+    return render_template("index.html", transaction_statment=transaction_statment_dict, weekly = weekly_expense|weekly_income, monthly = monthly_expense, halfyearly=halfyearly_expense)
+
+
+@app.route("/expense", methods=["GET", "POST"])
+@login_required
+def expense():
+    """Show expense form"""
+    
+    categories = []
+    for item in db.execute("SELECT category FROM categories"):
+        categories.append(item['category'])
+    
+    if request.method == "POST":
+        id  = session["user_id"]
+        description = request.form.get("description")
+        amount = request.form.get("amount")
+        category = request.form.get("category")
+        
+        # Perform input validation
+        if not description:
+            return apology("Please enter description")
+        if not amount.isnumeric():
+            return apology("Amount must be Numeric only")
+        amount = float(amount)
+        if amount < 0:
+            return apology("Amount must be non negative!")
+        if category not in categories:
+            return apology("Invalid category")
+        
+        db.execute("INSERT INTO expenses (user_id, description, category, amount) VALUES (?,?,?,?)", id, description, category, amount)
+        flash("Expense added!")
+        return redirect("/")
+    
+    return render_template("expense.html", categories = categories)
+
+@app.route("/income", methods=["GET", "POST"])
+@login_required
+def income():
+    if request.method == "POST":
+        id  = session["user_id"]
+        description = request.form.get("description")
+        amount = request.form.get("amount")
+        
+        # Perform input validation
+        if not description:
+            return apology("Please enter description")
+        if not amount.isnumeric():
+            return apology("Amount must be Numeric only")
+        amount = float(amount)
+        if amount < 0:
+            return apology("Amount must be non negative!")
+        
+        db.execute("INSERT INTO income (user_id, description, amount) VALUES (?,?,?)", id, description, amount)
+        flash("Income added!")
+        return redirect("/")
+    
+    return render_template("income.html")
---
+++ helpers.py
@@ -0,0 +1,153 @@
+from flask import redirect, session, render_template
+from functools import wraps
+from cs50 import SQL
+from datetime import date
+
+import locale
+locale.setlocale(locale.LC_MONETARY, 'en_IN')
+
+db = SQL("sqlite:///easebudget.db")
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def apology(msg, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", err=code, message=msg), code
+
+def get_expense(id,day):
+    """Returns expense of a particular user."""
+    category_expense = db.execute(
+        f"SELECT category, SUM(amount) AS 'amount' FROM expenses WHERE user_id = ? AND DATE(date_time) > DATE(DATE(), '-{day} day') GROUP BY category ORDER BY amount DESC", id)
+    category = db.execute("SELECT category FROM categories")
+    category = [x['category'] for x in category]
+    for item in category_expense:
+        category.remove(item['category'])
+    for item in category:
+            category_expense.append({'category':item, 'amount':0})
+    
+    expense_list = db.execute(f"""SELECT date_time, DATE(date_time) as date, description, category, amount
+                                    FROM expenses 
+                                    WHERE user_id = ? AND DATE(date_time) > DATE(DATE(), '-{day} day') 
+                                    ORDER BY date_time DESC""", id)
+    
+    expense_sum = 0
+    for item in expense_list:
+        expense_sum += item["amount"]
+    
+    expense_history_day = []
+    expense_history_amt = []
+    # Get history for the week
+    if day == 7:
+        expense_history = db.execute(f"""SELECT CASE CAST (STRFTIME('%w',date_time) AS INTEGER) when 0 then 'Sun'
+                                            when 1 then 'Mon'
+                                            when 2 then 'Tue'
+                                            when 3 then 'Wed'
+                                            when 4 then 'Thu'
+                                            when 5 then 'Fri'
+                                            else 'Sat' end as weekday, SUM(amount) AS amount 
+                                            FROM expenses 
+                                            WHERE user_id = ? AND DATE(date_time) > DATE(DATE(), '-{day} day') 
+                                            GROUP BY DATE(date_time)
+                                            ORDER BY DATE(date_time)""", id)
+        
+        day_dic= {'Sun':0,'Mon':1, 'Tue':2, 'Wed':3, 'Thu':4, 'Fri':5, 'Sat':6}
+        for item in expense_history:
+           day_dic.pop(item['weekday'])
+        for item in day_dic:
+            expense_history.insert(day_dic[item],{'weekday':item, 'amount':0})
+        
+        for item in expense_history:
+            expense_history_day.append(item['weekday'])
+            expense_history_amt.append(item['amount'])
+       
+    # expense_dict = {}
+    # for item in expense_list:
+    #     if item["date"] in expense_dict:
+    #         expense_dict[item["date"]].append(item)
+    #     else:
+    #         expense_dict[item["date"]]= [item,]
+        
+    category_lst = []
+    category_amt = []
+    for item in category_expense:
+        category_lst.append(item['category'])
+        category_amt.append(item['amount'])
+        
+        
+    return {"expense_list":expense_list, "expense_sum":expense_sum, "category_expense":category_expense, "category_lst":category_lst, "category_amt":category_amt, "expense_history_day":expense_history_day, "expense_history_amt":expense_history_amt}
+
+def get_income(id,day):
+    income_list = db.execute(f"""SELECT date_time, DATE(date_time) as date, description, category, amount, SUM(amount) AS sum_amount
+                                    FROM income 
+                                    WHERE user_id = ? AND DATE(date_time) > DATE(DATE(), '-{day} day') 
+                                    ORDER BY date_time DESC""", id)
+    income_sum = 0
+    for item in income_list:
+        income_sum += item["amount"]
+    return{"income_list":income_list, "income_sum":income_sum}
+
+
+def get_transaction_statment(expense_list, income_list):
+    transaction_statment = []
+    transaction_statment.extend(expense_list)
+    transaction_statment.extend(income_list)
+    sorted_transaction_statement = sorted(transaction_statment, key=lambda x: x['date_time'], reverse=True)
+    
+    for item in sorted_transaction_statement:
+        if item["category"] != "Income":
+            item["amount"] =  -item["amount"]
+    
+    transaction_statment_dict = {} 
+    for item in sorted_transaction_statement:
+        if item["date"] in transaction_statment_dict:
+            transaction_statment_dict[item["date"]].append(item)
+        else:
+            transaction_statment_dict[item["date"]]= [item,]
+    return transaction_statment_dict
+    
+
+def inr(value):
+    """Format value as INR."""
+    if value < 0:
+        value = abs(value)
+        return "-  "+ locale.currency(value, grouping=True)
+        
+    return locale.currency(value, grouping=True)
+
+def format_date(value):
+    """Format date"""
+    return date.fromisoformat(value).strftime("%a, %d %b %y")
